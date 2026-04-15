59682f9912285595fa343a2a3ed86f84b2fb9339
zehrei verj morales <moraleszehreiverj8@gmail.com>
2026-04-15 23:47:35 +0800

    setup express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,14 @@
-const test = "Hello, World!";
-console.log(test);
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
