5bb75b60abb4eb47e7cc6caf5f72e37ca058ec85
zehrei verj morales <moraleszehreiverj8@gmail.com>
2026-04-16 01:05:33 +0800

    simple chat

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,30 @@
 import express from "express";
+import OpenAI from "openai";
+
+const client = new OpenAI({
+    baseURL: process.env.LLM_URL,
+    apiKey: process.env.API_KEY,
+});
+const llmModel = process.env.LLM_MODEL || "smollm2:latest";
 
 const app = express();
-
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Hello, World!");
+});
+
+app.post("/api/v1/chat", async (req, res) => {
+    if (!req.body.input || req.body.input.length === 0) {
+        return res.status(400).send("Input is required");
+    }
+
+    const response = await client.chat.completions.create({
+        model: llmModel,
+        messages: [{ role: "user", content: req.body.input }],
+    });
+
+    return res.send(response.choices[0]?.message.content);
 });
 
 const PORT = process.env.PORT || 3000;
